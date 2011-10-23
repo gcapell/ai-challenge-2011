085f25f649add59da3171459a58303797fc9197d
Gary Capell <gary.capell@gmail.com>
2011-10-23 15:16:31 +1100

    Explores

--- MyBot.go
+++ MyBot.go
@@ -11,6 +11,7 @@ type MyBot struct {
 
 const (
 	FOOD_DEPTH = 7
+	EXPLORE_DEPTH = 10
 )
 
 var (
@@ -28,15 +29,19 @@ func NewBot(s *State) Bot {
 //DoTurn is where you should do your bot's actual work.
 func (mb *MyBot) DoTurn(s *State) os.Error {
 
-	mb.moveToNearbyFood(s, s.Map)
+	// Grab nearby food
+	mb.moveToTarget(s, s.Map, "food", FOOD_DEPTH, func(loc Location) bool { return s.Map.Food[loc]})
+
+	// Explore the unknown
+	mb.moveToTarget(s, s.Map, "explore", EXPLORE_DEPTH, func(loc Location) bool { return s.Map.itemGrid[loc] == UNKNOWN})
+
 	mb.moveRandomly(s)
 	//returning an error will halt the whole program!
 	return nil
 }
-
-// If there's food nearby an ant, move towards it
+// If there's some useful target nearby an ant, move towards it
 // Breadth first search
-func (mb *MyBot) moveToNearbyFood(s *State, m *Map) {
+func (mb *MyBot) moveToTarget(s *State, m *Map, reason string, depth int, isTarget func(Location)bool) {
 	seen := make(map[Location]bool)
 
 	frontier := make(map[Location]Move)	// current -> src
@@ -48,8 +53,9 @@ func (mb *MyBot) moveToNearbyFood(s *State, m *Map) {
 
 	moved := make(map[Location]bool)
 
-	for iter:=0; iter<FOOD_DEPTH; iter++ {
-		log.Printf("frontier %d, %v\n", len(frontier), frontier)
+	iter:= 0
+	maxFrontier := 0
+	for ; iter<depth; iter++ {
 
 		newFrontier := make(map[Location]Move)
 		newMoved := make(map[Location]bool)
@@ -59,7 +65,7 @@ func (mb *MyBot) moveToNearbyFood(s *State, m *Map) {
 			initialMove := first.d == NoMovement
 
 			// Ignore ants who moved last go
-			if moved[first.src] {
+			if moved[first.src] || newMoved[first.src] {
 				continue
 			}
 			
@@ -71,8 +77,8 @@ func (mb *MyBot) moveToNearbyFood(s *State, m *Map) {
 					first.d = d
 				}
 				seen[next] = true
-				if m.Food[next] {
-					log.Printf("moving %v for food\n", first)
+				if isTarget(next) {
+					log.Printf("moving %v for %s\n", first, reason)
 					s.IssueOrderLoc(first.src, first.d)
 					newMoved[first.src] = true
 					break
@@ -81,11 +87,18 @@ func (mb *MyBot) moveToNearbyFood(s *State, m *Map) {
 				}
 			}
 		}
+		frontierSize := len(newFrontier)
+		if frontierSize == 0 {
+			break
+		}
+		if frontierSize > maxFrontier {
+			maxFrontier = frontierSize
+		}
 		frontier = newFrontier
 		moved = newMoved
 	}
+	log.Printf("Searched to depth %d, width %d for  %s\n", iter, maxFrontier, reason)
 }
-
 
 // Any ants not yet moving should move randomly
 func (mb *MyBot) moveRandomly(s *State) {
