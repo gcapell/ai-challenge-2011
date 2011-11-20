ea3e051a8e464b5706fc79a51353fea824acb128
Gary Capell <gary.capell@gmail.com>
2011-11-21 10:42:34 +1100

    Working on path2

--- path2.go
+++ path2.go
@@ -7,67 +7,77 @@ import (
 	"fmt"
 )
 
+const (
+	EXPN_NONE = iota
+	EXPN_HORIZONTAL
+	EXPN_VERTICAL
+)
+
 type (
-	Node struct {
+	Node2 struct {
 		estimate float64
 		length   int
-		p        Point
+		Point
+		prev	*Node2	// backlinks to reconstruct path
+		expanded
 	}
 
-	myHeap struct {
+	myHeap2 struct {
 		vector.Vector
 	}
 )
 
-func (h *myHeap) Less(i, j int) bool { return h.At(i).(Node).estimate < h.At(j).(Node).estimate }
+func (h *myHeap) Less(i, j int) bool { return h.At(i).(Node2).estimate < h.At(j).(Node2).estimate }
 
-func reverse(points []Point) {
-	for j, k := 0, len(points)-1; j < k; j, k = j+1, k-1 {
-		points[k], points[j] = points[j], points[k]
-	}
-}
+func (src Point) ShortestPath2(dst Point, m *Map) ([]Point, os.Error) {
 
-func unravelPath(back []Location, src, dst Point, pathLength int) []Point {
-	// Follow breadcrumbs from 'dst' to 'src'
-	reply := make([]Point, 0, pathLength)
-	for p := dst; !p.Equals(src); p = back[p.loc()].point() {
-		reply = append(reply, p)
-	}
-	reverse(reply)
-	return reply
-}
-
-func (src Point) ShortestPath(dst Point, m *Map) ([]Point, os.Error) {
-
-	h := &myHeap{}
+	h := &myHeap2{}
 	heap.Init(h)
 
-	heap.Push(h, Node{src.CrowDistance(dst), 0, src})
+	heap.Push(h, Node{src.CrowDistance(dst), 0, src, nil})
 
 	// Each entry points to previous point in path
-	back := make([]Location, ROWS*COLS)
-
-	INVALID_LOC := Location(ROWS * COLS + 1)
-
-	for j := 0; j < len(back); j++ {
-		back[j] = INVALID_LOC
-	}
+	seen := make(map[Location]bool)
 
 	for h.Len() != 0 {
-		n := heap.Pop(h).(Node)
-		pathLength := n.length + 1
-		for _, p := range m.AccessibleNeighbours(n.p) {
-			// Have we already seen this point?
-			if back[p.loc()] != INVALID_LOC {
+		n := heap.Pop(h).(Node2)
+		for n2 := range n.expand(m, dst) {
+			if seen[n2.loc()] {
 				continue
 			}
-			newNode := Node{p.CrowDistance(dst) + float64(pathLength), pathLength, p}
-			back[p.loc()] = n.p.loc()
-			if p.Equals(dst) {
-				return unravelPath(back, src, dst, pathLength), nil
+			if n2.Equals(dst) {
+				return n2.path()
 			}
 			heap.Push(h, newNode)
 		}
+		}
 	}
 	return nil, fmt.Errorf("no path found")
+}
+
+func (n *Node2) expand(m *Map, dst Point) chan *Node2 {
+	c := make(chan *Node2)
+	if n.expanded != EXPN_VERTICAL {
+		go n.expandDir(m, dst, c, EAST)
+		go n.exapndDir(m, dst, c, WEST)
+	}
+	if n.expanded != EXPN_HORIZONTAL {
+		go n.expandDir(m, dst, c, NORTH)
+		go n.expandDir(m, dst, c, SOUTH)
+	}
+	return c
+	newNode := Node{p.CrowDistance(dst) + float64(pathLength), pathLength, p}
+	
+}
+
+func (n *Node2) expandDir(m *Map, dst Point, c chan *Node2, d Direction) {
+	p := n.Point
+	for p:= n.Point; !m.isWet(p); p := d.East(p) {
+		for _, d2 := range d.Orthogonal() {
+		for p2 := d.North(p); !m.isWet(p); p2 := d.North(p2){
+			if isCorner(p2, d, m, dst) {
+				c <- n.Child(p2, dst, d)
+			}
+		}
+	}
 }
